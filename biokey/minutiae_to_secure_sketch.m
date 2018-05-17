e7f57ef45db917cbca56a927c3adeffad26aba2e
kozyilmaz <kazim@monolytic.com>
2018-05-17 18:05:36 +0300

    up

---
+++ biokey/minutiae_to_secure_sketch.m
@@ -0,0 +1,20 @@
+% Kazım Rıfat Özyılmaz
+% 2016800027
+% kazim.ozyilmaz@boun.edu.tr - kazim@monolytic.com
+%
+% Creating Secure Sketches from Fingerprint Minutiae Data
+%
+% output:
+%   x: random data
+%   s: secure sketch
+%   R: hash result
+
+function [a, b, c] = minutiae_to_secure_sketch(d)
+
+% load extracted minutiae data from FVC2002 database
+load('db.mat');
+
+
+
+
+end
