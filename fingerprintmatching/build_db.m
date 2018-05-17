42eae895f66a2b818b5b428cf893113d7199708f
kozyilmaz <kazim@monolytic.com>
2018-05-17 12:59:52 +0300

    updated db

--- fingerprintmatching/build_db.m
+++ fingerprintmatching/build_db.m
@@ -16,7 +16,7 @@ function build_db(ICount, JCount)
     p=0;
     for i=1:ICount
         for j=1:JCount
-            filename=['10' num2str(i) '_' num2str(48) '.tif'];
+            filename=['10' num2str(i) '_' num2str(j) '.tif'];
             img = imread(filename); p=p+1;
             if ndims(img) == 3; img = rgb2gray(img); end   % colour image
             disp(['extracting features from ' filename ' ...']);
